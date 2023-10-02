c8b809ac2a7f95d77aabce3328ba328019dc38c2
AlejandroV074 <alejandrovargascuartas@gmail.com>
2023-10-01 23:15:12 -0300

    style: navbar responsive

--- src/frontend/navbar/navbar.js
+++ src/frontend/navbar/navbar.js
@@ -3,38 +3,48 @@ import './navbar.css';
 
 function Navbar() {
     const [showHolyLabel, setShowHolyLabel] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
 
     useEffect(() => {
-        // Detecta el evento de desplazamiento
         const handleScroll = () => {
             if (window.scrollY > 100) {
-                // Muestra el elemento "HolyLabel" después de desplazarse 100 píxeles
                 setShowHolyLabel(true);
             } else {
-                // Oculta el elemento "HolyLabel" si se desplaza hacia arriba
                 setShowHolyLabel(false);
             }
         };
 
-        // Agrega un event listener para el evento de desplazamiento
         window.addEventListener('scroll', handleScroll);
 
-        // Limpia el event listener cuando se desmonta el componente
         return () => {
             window.removeEventListener('scroll', handleScroll);
         };
     }, []);
 
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
+
     return (
         <nav>
-            <ul>
+            <div className={`menu-btn ${showMenu ? 'active' : ''}`} onClick={toggleMenu}>
+                <div className={`bar ${showMenu ? 'active' : ''}`}></div>
+                <div className={`bar ${showMenu ? 'active' : ''}`}></div>
+                <div className={`bar ${showMenu ? 'active' : ''}`}></div>
+            </div>
+            <ul className={`menu ${showMenu ? 'active' : ''}`}>
                 {showHolyLabel && (
-                    <li className="special-item"><a href="/">HolyLabel</a></li>
+                    <li className="special-item">
+                        <a href="/" className="logo-label-container">
+                            <img src="/images/holy.png" alt="Logo de HolyLabel" className="logo" />
+                            <span>HolyLabel</span>
+                        </a>
+                    </li>
                 )}
                 <li><a href="/">Inicio</a></li>
                 <li><a href="/">¿Quiénes somos?</a></li>
-                <li><a href="/acerca-de">FAQ</a></li>
-                <li><a href="/contacto" className="scan-qr">Escanear QR</a></li>
+                <li><a href="/">FAQ</a></li>
+                <li><a href="/" className="scan-qr">Escanear QR</a></li>
             </ul>
         </nav>
     );
