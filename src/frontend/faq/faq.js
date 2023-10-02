1f1653ee2378fafa67bc2eddb807a5d203a5d66f
AlejandroV074 <alejandrovargascuartas@gmail.com>
2023-10-01 21:59:48 -0300

    styles: se agrega responsive en sección de preguntas, se añaden estilos

--- src/frontend/faq/faq.js
+++ src/frontend/faq/faq.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './faq.css';
 
 function Faq({ pregunta, respuesta }) {
     const [expandir, setExpandir] = useState(false);
@@ -7,39 +8,18 @@ function Faq({ pregunta, respuesta }) {
         setExpandir(!expandir);
     };
 
-    const faqStyle = {
-        color: '#000',
-        fontFamily: 'Montserrat',
-        fontSize: '24px',
-        fontStyle: 'normal',
-        fontWeight: 500,
-        lineHeight: 'normal',
-        margin: '30px 0', 
-        padding: '30px', 
-        borderRadius: '8px',
-        background: '#FFF',
-        boxShadow: '1px 5px 4px 0px #D4AF37',
-        transition: 'box-shadow 0.3s ease',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        cursor: 'pointer',
-    };
-
-    const respuestaStyle = {
-        padding: '20px',
-        borderRadius: '8px',
-    };
-
     return (
-        <div style={faqStyle} onClick={toggleExpandir}>
+        <div className={`faq-container ${expandir ? 'expanded' : ''}`} onClick={toggleExpandir}>
             <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
                 <span>{pregunta}</span>
-                <span style={{ fontSize: '20px', marginLeft: '8px' }}>{expandir ? '▼' : '▶'}</span>
+                <span className="arrow-icon">{expandir ? '▼' : '▶'}</span>
             </div>
             {expandir && (
-                <div style={respuestaStyle}>
-                    {respuesta}
+                <div>
+                    <div className="faq-line"></div>
+                    <div className="faq-answer">
+                        {respuesta}
+                    </div>
                 </div>
             )}
         </div>
