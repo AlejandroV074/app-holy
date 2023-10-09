18b1b6c59b48a892b7887bf7866feb4fcbbae7b8
AlejandroV074 <alejandrovargascuartas@gmail.com>
2023-10-09 01:53:40 -0300

    fix: se corrigen carpetas y estructura adecuada

--- src/App.js
+++ src/App.js
@@ -1,65 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import Navbar from './frontend/components/navbar/navbar';
-import Forma from './frontend/components/form/form';
-import Faq from './frontend/components/faq/faq';
-import Footer from './frontend/components/footer/footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './frontend/pages/home/home';
+import Login from './frontend/components/login/login';
 
 function App() {
-  const [showHolyLabel, setShowHolyLabel] = useState(true);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 100) {
-        setShowHolyLabel(false);
-      } else {
-        setShowHolyLabel(true);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-
   return (
     <div className="App">
-      <header className="App-header">
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet" />
-
-        <div className={`large-text ${showHolyLabel ? 'show' : ''}`}>
-          HolyLabel
-
-        </div>
-        <Navbar />
-      </header>
-
-      <main>
-        <Forma backgroundImage="/images/person.png" texto="¿Quiénes somos?"
-        
-          textoAdicional="Nuestra plataforma utiliza avanzada tecnología de 
-          validación y registro mediante códigos QR personalizados. Además, 
-          implementamos la consulta de trazabilidad en una cadena de blockchain, 
-          asegurando así la transparencia y seguridad en cada paso de la cadena de 
-          suministro de las bebidas alcohólicas." />
-
-
-        <div className='tituloStyle'>FAQ</div>
-        <Faq
-          pregunta="¿Cómo funciona la aplicación?"
-          respuesta="La aplicación funciona de la siguiente manera: "
-        />
-        <Faq
-          pregunta="¿Cómo funciona la aplicación?"
-          respuesta="La aplicación funciona de la siguiente manera: "
-        />
-        <Footer />
-      </main>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
+
 
 export default App;
