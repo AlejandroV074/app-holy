8b18bccd13a47d3e3df97f98ebe26430c796eccb
AlejandroV074 <alejandrovargascuartas@gmail.com>
2023-10-09 20:53:44 -0300

    fix: implementación de api

--- src/frontend/components/login/login.js
+++ src/frontend/components/login/login.js
@@ -8,7 +8,7 @@ class Login extends Component {
         this.state = {
             username: '',
             password: '',
-            userType: '',
+            type_user: '',
         };
     }
 
@@ -19,11 +19,26 @@ class Login extends Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log('Nombre de usuario:', this.state.username);
-        console.log('Contraseña:', this.state.password);
-        console.log('Tipo de usuario:', this.state.userType);
-        // Aquí puedes agregar la lógica de autenticación, como enviar datos al servidor
-        // o autenticar al usuario localmente.
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({
+            "user": this.state.username,
+            "password": this.state.password,
+            "type_user": this.state.type_user
+        });
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        fetch("https://holylabelapi.azurewebsites.net/api-token-auth/", requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
     }
 
     render() {
@@ -42,46 +57,46 @@ class Login extends Component {
                         <div className="card card-style">
                             <div className="card-body">
                                 <form onSubmit={this.handleSubmit} className='form-style'>
-                                <div className='login'>
-                                <h2 className="card-title title-style">Iniciar Sesión</h2>
-                                <img src="/images/holy-hd.png" alt="Logo de HolyLabel" className="logo-login"/>
-                                <h2 className="title-style-log" >HolyLabel</h2>
-                                    <div className="mb-3">
-                                        <input
-                                            type="text"
-                                            name="username"
-                                            value={this.state.username}
-                                            onChange={this.handleInputChange}
-                                            className="form-control input-style"
-                                            placeholder="Nombre de usuario"
-                                        />
+                                    <div className='login'>
+                                        <h2 className="card-title title-style">Iniciar Sesión</h2>
+                                        <img src="/images/holy-hd.png" alt="Logo de HolyLabel" className="logo-login" />
+                                        <h2 className="title-style-log" >HolyLabel</h2>
+                                        <div className="mb-3">
+                                            <input
+                                                type="text"
+                                                name="username"
+                                                value={this.state.username}
+                                                onChange={this.handleInputChange}
+                                                className="form-control input-style"
+                                                placeholder="Nombre de usuario"
+                                            />
+                                        </div>
+                                        <div className="mb-3">
+                                            <input
+                                                type="password"
+                                                name="password"
+                                                value={this.state.password}
+                                                onChange={this.handleInputChange}
+                                                className="form-control input-style"
+                                                placeholder="Contraseña"
+                                            />
+                                        </div>
+                                        <div className="mb-3">
+                                            <select
+                                                name="type_user"
+                                                value={this.state.type_user}
+                                                onChange={this.handleInputChange}
+                                                className="form-select input-style"
+                                            >
+                                                <option value="" disabled selected>Tipo de Usuario</option>
+                                                <option value="Decanter">Licoreras</option>
+                                                <option value="Factory">Fabricantes</option>
+                                                <option value="Distributor">Distribuidoras</option>
+                                            </select>
+                                        </div>
+                                        <a className='f-password'>Olvidé mi contraseña</a>
+                                        <button type="submit" className="btn btn-primary">Iniciar Sesión</button>
                                     </div>
-                                    <div className="mb-3">
-                                        <input
-                                            type="password"
-                                            name="password"
-                                            value={this.state.password}
-                                            onChange={this.handleInputChange}
-                                            className="form-control input-style"
-                                            placeholder="Contraseña"
-                                        />
-                                    </div>
-                                    <div className="mb-3">
-                                        <select
-                                            name="userType"
-                                            value={this.state.userType}
-                                            onChange={this.handleInputChange}
-                                            className="form-select input-style"
-                                        >
-                                            <option value="" disabled selected>Tipo de Usuario</option>
-                                            <option value="Licoreras">Licoreras</option>
-                                            <option value="Fabricantes">Fabricantes</option>
-                                            <option value="Distribuidoras">Distribuidoras</option>
-                                        </select>
-                                    </div>
-                                    <a className='f-password'>Olvidé mi contraseña</a>
-                                    <button type="submit" className="btn btn-primary">Iniciar Sesión</button>
-                                </div>
                                 </form>
                             </div>
                         </div>
