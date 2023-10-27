493417f48361b5f77924fafef3dc09206d730352
AlejandroV074 <alejandrovargascuartas@gmail.com>
2023-10-27 20:57:16 -0300

    fix: se aregla error en diseño

--- src/frontend/pages/login/login.js
+++ src/frontend/pages/login/login.js
@@ -52,7 +52,7 @@ class Login extends Component {
 
     render() {
         return (
-            <div className="container mt-5">
+            <div className="">
                 <Navbar />
                 <div className="row justify-content-center">
                     <div className="col-md-6">
