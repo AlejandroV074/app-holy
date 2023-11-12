94f5ebe10c420c42df8c7525dacbfe2466fa3bbb
AlejandroV074 <alejandrovargascuartas@gmail.com>
2023-11-12 19:45:32 -0300

    add: se agrega style y api

--- src/frontend/components/crear-botellas/constants.js
+++ src/frontend/components/crear-botellas/constants.js
@@ -1,21 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './styles.css';
-import { Link } from 'react-router-dom';
 
 function Presentation() {
-    const options = ["Ron Medellín añejo 3 años", "Ron Medellín dorado", "Medellín crema de ron 8 años"];
-    const [searchTerm, setSearchTerm] = useState('');
+    const [searchTerm, setSearchTerm] = useState("");
+    const [options, setOptions] = useState([]);
     const [searchResults, setSearchResults] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        fetch("https://holylabelapi.azurewebsites.net/presentacion/")
+            .then((response) => response.json())
+            .then((data) => {
+                const presentaciones = data.map((presentacion) => presentacion);
+                setOptions(presentaciones);
+            })
+            .catch((error) => console.error("Error fetching data from API:", error));
+    }, []);
 
     const handleInputChange = (event) => {
         const term = event.target.value;
         setSearchTerm(term);
 
-        const results = options.filter(option =>
-            option.toLowerCase().includes(term.toLowerCase())
+        const results = options.filter(
+            (option) =>
+                option && option.name.toLowerCase().includes(term.toLowerCase())
         );
 
         setSearchResults(results);
+    };
+
+    const handleResultClick = (presentation) => {
+        navigate(`/contenido/${presentation.id_presentation}`, {
+            state: { presentation },
+        });
     };
 
     return (
@@ -26,21 +44,25 @@ function Presentation() {
                 value={searchTerm}
                 onChange={handleInputChange}
             />
-                {searchTerm.length > 0 && (
-                    <div className="search-results">
-                        {searchResults.map((result, index) => (
-                            <div key={index}>{result}</div>
-                        ))}
-                        {searchResults.length === 0 && searchTerm.length > 0 && (
-                            <div style={{textAlign: 'center'}}>No existen resultados</div>
-                        )}
-                        <ul>
-                            <li>
-                                <Link to="/crear-botellas">Crear nueva presentación</Link>
-                            </li>
-                        </ul>
-                    </div>
-                )}
+            {searchTerm.length > 0 && (
+                <div className="search-results" style={{ borderRadius: '10px', padding: '65px', backgroundColor: 'white', boxShadow: '1px 5px 4px 0px #d4af37', height: '230px' }}>
+                    {searchResults.map((result) => (
+                        <div key={result.id_presentation} style={{ marginBottom: '2%', marginTop: '5%' }}>
+                            <div style={{ color: 'black', cursor: 'pointer' }} onClick={() => handleResultClick(result)}>
+                                {result.name}
+                            </div>
+                        </div>
+                    ))}
+                    {searchResults.length === 0 && searchTerm.length > 0 && (
+                        <div style={{ textAlign: 'center', color: 'black', marginTop: '20%' }}>No existen resultados</div>
+                    )}
+                    <ul>
+                        <li>
+                            <Link to="/crear-botellas">Crear nueva presentación</Link>
+                        </li>
+                    </ul>
+                </div>
+            )}
         </div>
     );
 }
